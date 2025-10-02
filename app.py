afe5e3d222209fc76f78abc1441ef5cb53704b61
OmkarBhujbalra0 <omkarbhujbalrao6@gmail.com>
2025-10-02 15:39:37 +0530

    'Session Addition Try 1'

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session
 import numpy as np
 import pandas as pd
 
@@ -12,7 +12,22 @@ def home():
 
 @app.route('/upload')
 def upload():
+    if request.method == 'POST':
+         if 'submit' in request.form:
+        file = request.files['csvfile']
+        if file and file.filename.endswith('.csv'):
+            data = pd.read_csv(file)
+            session['data'] = data.to_json()
+    elif 'next' in request.form:
+        if 'data' in session:
+            return redirect(url_for('/clean'))
+        else:
+            return "No Data Uploaded! Please upload to proceed"
     return render_template('inputpage.html')
+
+@app.route('/clean')
+def clean():
+    return render_template('cleaning.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
