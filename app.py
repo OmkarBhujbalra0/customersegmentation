99e42674b421e198f9334baf6322248a56d4b58b
OmkarBhujbalra0 <omkarbhujbalrao6@gmail.com>
2025-10-04 19:15:32 +0530

    'Input and Output'

--- app.py
+++ app.py
@@ -1,33 +1,28 @@
-from flask import Flask,render_template,session
-import numpy as np
+from flask import Flask,render_template,request,redirect,url_for
 import pandas as pd
-
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/upload')
+@app.route("/upload",methods=['GET','POST'])
 def upload():
     if request.method == 'POST':
-         if 'submit' in request.form:
-        file = request.files['csvfile']
-        if file and file.filename.endswith('.csv'):
-            data = pd.read_csv(file)
-            session['data'] = data.to_json()
-    elif 'next' in request.form:
-        if 'data' in session:
-            return redirect(url_for('/clean'))
-        else:
-            return "No Data Uploaded! Please upload to proceed"
-    return render_template('inputpage.html')
-
-@app.route('/clean')
-def clean():
-    return render_template('cleaning.html')
+        file = request.files.get('csvfile')
+        if file:
+            if file.filename.endswith('.csv'):
+                data = pd.read_csv(file)
+                print(f"File uploaded successfully.{data.shape[0]} rows and {data.shape[1]} columns")
+                table_string = data.to_html()
+                return render_template('inputpage.html',message= f"File uploaded successfully.{data.shape[0]} rows and {data.shape[1]} columns",table_string=table_string)
+            else:
+                print("Please Upload a Valid CSV File")
+                return render_template('inputpage.html',message="Please Upload a Valid CSV File")
+    return render_template("inputpage.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# Use Flash function for message in next part
